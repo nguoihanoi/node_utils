077b2fc31442237296c62181948ab7610015bbc7
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-05-10 15:10:45 +0700

    Update language.ts

--- src/libs/language.ts
+++ src/libs/language.ts
@@ -2,21 +2,29 @@ import { printf } from 'fast-printf';
 import * as path from "path";
 import i18next from 'i18next';
 import backendI18 from "i18next-fs-backend";
+let initStatus = false;
 
-try {
-    i18next.use(backendI18).init({
-        initImmediate: false,
-        backend: {
-            loadPath: path.join(path.resolve(path.resolve(__dirname, ".."), ".."), "locales") + '/{{lng}}.json',
-        },
-        fallbackLng: 'vi',
-        preload: ['en', 'vi']
-    });
-} catch (error) {
-    console.error(error);
+const initData = () => {
+    if (initStatus == false)
+        try {
+            const pathText = path.join(path.resolve(path.resolve(__dirname, ".."), ".."), "locales") + '/{{lng}}.json';
+            console.log(pathText);
+            i18next.use(backendI18).init({
+                initImmediate: false,
+                backend: {
+                    loadPath: pathText,
+                },
+                fallbackLng: 'vi',
+                preload: ['en', 'vi']
+            });
+            initStatus = true;
+        } catch (error) {
+            console.error(error);
+        }
 }
 
 export const oneText = (inText: string, inLang: string = "vi") => {
+    initData();
     return i18next.t(inText, { lng: inLang });
 };
 export const replaceText = (inText: string, ...inValue: string[]) => {
