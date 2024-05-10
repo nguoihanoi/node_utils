cefd299e8d21f9509455cf6e8e7e20e81f408205
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-05-10 15:56:23 +0700

    Update language.ts

--- src/libs/language.ts
+++ src/libs/language.ts
@@ -1,10 +1,9 @@
 import { printf } from 'fast-printf';
 import * as path from "path";
 import * as i18next from 'i18next';
-import backendI18 from "i18next-fs-backend";
 let initStatus = false;
 
-const initData = () => {
+const initData = (backendI18: any) => {
     if (initStatus == false)
         try {
             const pathText = path.join(path.resolve(path.resolve(path.resolve(path.resolve(__dirname, ".."), ".."), ".."), ".."), "locales") + '/{{lng}}.json';
@@ -22,8 +21,9 @@ const initData = () => {
         }
 }
 
-export const oneText = (inText: string, inLang: string = "vi") => {
-    initData();
+export const oneText = (inText: string, inLang: string = "vi", inputInit: any = false) => {
+    if (inputInit != false)
+        initData(inputInit);
     return i18next.t(inText, { lng: inLang });
 };
 export const replaceText = (inText: string, ...inValue: string[]) => {
