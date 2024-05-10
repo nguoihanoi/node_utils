e2778ace5b5756a45a36bac76458cde8ad0a1bc4
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-05-10 16:16:32 +0700

    Update language.d.ts

--- lib/libs/language.d.ts
+++ lib/libs/language.d.ts
@@ -1,2 +1,3 @@
+export declare const initData: (inputInit: any) => void;
 export declare const oneText: (inText: string, inLang?: string) => string;
 export declare const replaceText: (inText: string, ...inValue: string[]) => string;
