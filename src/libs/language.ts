bc75f1ea6a7af427b9c7fbd86afcf6d5db5058fd
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-05-10 13:46:09 +0700

    Update language.ts

--- src/libs/language.ts
+++ src/libs/language.ts
@@ -3,14 +3,18 @@ import * as path from "path";
 import i18next from 'i18next';
 import backendI18 from "i18next-fs-backend";
 
-i18next.use(backendI18).init({
-    initImmediate: false,
-    backend: {
-        loadPath: path.join(path.resolve(path.resolve(__dirname, ".."), ".."), "locales") + '/{{lng}}.json',
-    },
-    fallbackLng: 'vi',
-    preload: ['en', 'vi']
-});
+try {
+    i18next.use(backendI18).init({
+        initImmediate: false,
+        backend: {
+            loadPath: path.join(path.resolve(path.resolve(__dirname, ".."), ".."), "locales") + '/{{lng}}.json',
+        },
+        fallbackLng: 'vi',
+        preload: ['en', 'vi']
+    });
+} catch (error) {
+    console.error(error);
+}
 
 export const oneText = (inText: string, inLang: string = "vi") => {
     return i18next.t(inText, { lng: inLang });
